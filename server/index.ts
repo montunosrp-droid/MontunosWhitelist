b479bd4357bffdd7d4b51f0a3b31c0c14765999f
montunosrp-droid <montunosrp@gmail.com>
2025-11-12 05:19:09 +0000

    added health route for render

--- server/index.ts
+++ server/index.ts
@@ -96,6 +96,11 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || "5000", 10);
+
+  app.get("/health", (_req, res) => {
+    res.send("ok");
+  });
+
   server.listen(
     {
       port,
